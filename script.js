ac533ab32cf6e15975daa0acceee245d77581f41
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-16 22:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function shortcut(s1, s2) {
   // your code here
+	if (!s1 || !s2) {
+    // If either string is empty, return an empty string.
+    return '';
+  }
+  // Get the first character of each string.
+  const firstChar1 = s1[0];
+  const firstChar2 = s2[0];
+  // Concatenate the first characters of the two strings and return the result.
+  return firstChar1 + firstChar2;
 }
 
 // Do not change the code below.
